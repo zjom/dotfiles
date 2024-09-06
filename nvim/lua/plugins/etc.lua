602370d9e75c42c4722976827208f510662c1172
Zihan Jin <admin@zihanjin.com>
2024-09-06 12:31:30 +1000

    use copilot.lua instead of copilot.vim

--- nvim/lua/plugins/etc.lua
+++ nvim/lua/plugins/etc.lua
@@ -58,6 +58,11 @@ return {
 	},
 
 	{
-		"github/copilot.vim",
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		config = function()
+			require("copilot").setup({})
+		end,
 	},
 }
