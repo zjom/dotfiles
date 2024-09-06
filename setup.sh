8bde229fc666dbe459e348cd77b21ccfe601e33c
Zihan Jin <admin@zihanjin.com>
2024-09-06 12:22:32 +1000

    add tms

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/aerospace/aerospace.toml ~/.aerospace.toml
+
 ln -sf ~/dotfiles/nvim/ ~/.config/nvim
 ln -sf ~/dotfiles/wezterm/ ~/.config/wezterm
 ln -sf ~/dotfiles/tmux/ ~/.config/tmux
 ln -sf ~/dotfiles/bat/ ~/.config/bat
 ln -sf ~/dotfiles/gh/ ~/.config/gh
 ln -sf ~/dotfiles/git/ ~/.config/git
-ln -sf ~/dotfiles/aerospace/aerospace.toml ~/.aerospace.toml
-
+ln -sf ~/dotfiles/tms/ ~/.config/tms
