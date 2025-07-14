0f2937dbbcbf3b6387fdbbc3af8116374c79e10e
Zihan <admin@zihanjin.com>
2025-07-14 11:45:26 +1000

    use markdown tools

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -2,13 +2,3 @@
 vim.opt_local.spell = true
 vim.opt_local.signcolumn = "no"
 vim.g.markdown_fenced_languages = { "javascript", "typescript", "bash", "lua", "go", "rust", "c", "cpp" }
-
------- keymaps
-
-local map = function(mode, keys, func, desc)
-	vim.keymap.set(mode, keys, func, { buffer = true, desc = "Markdown: " .. desc })
-end
-
-map("n", "<C-k>", "z=", "Spell Suggest")
-
-map("n", "<leader>tp", ":PencilToggle<cr>", "[T]oggle [P]encil")
