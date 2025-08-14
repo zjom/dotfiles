a818c8aceef270f8a5184cf5a271969a6cf13e90
Zihan Jin <admin@zihanjin.com>
2025-08-15 01:29:56 +1000

    comment out unused themes in nvim

--- nvim/.config/nvim/lua/plugins/themes.lua
+++ nvim/.config/nvim/lua/plugins/themes.lua
@@ -12,19 +12,33 @@ return {
 			vim.cmd(":hi statusline guibg=NONE")
 		end,
 	},
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-	},
-	{
-		"nuvic/flexoki-nvim",
-		name = "flexoki",
-	},
-	{
-		"sainnhe/everforest",
-		config = function()
-			vim.g.everforest_background = "hard"
-			vim.g.everforest_better_performance = 1
-		end,
-	},
+	-- {
+	-- 	"ramojus/mellifluous.nvim",
+	-- 	name = "mellifluous",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		require("mellifluous").setup({
+	-- 			transparent = true,
+	-- 		})
+	--
+	-- 		vim.cmd("colorscheme mellifluous")
+	-- 		vim.cmd(":hi statusline guibg=NONE")
+	-- 	end,
+	-- },
+	-- {
+	-- 	"catppuccin/nvim",
+	-- 	name = "catppuccin",
+	-- },
+	-- {
+	-- 	"nuvic/flexoki-nvim",
+	-- 	name = "flexoki",
+	-- },
+	-- {
+	-- 	"sainnhe/everforest",
+	-- 	config = function()
+	-- 		vim.g.everforest_background = "hard"
+	-- 		vim.g.everforest_better_performance = 1
+	-- 	end,
+	-- },
 }
