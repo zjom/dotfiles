8d9edbf5a0f4d0eeceb0922d4b36a15191cf6395
Zihan Jin <admin@zihanjin.com>
2025-08-17 11:17:19 +1000

    nvim: use csharp_ls instead of omnisharp

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -209,7 +209,7 @@ return { -- LSP Configuration & Plugins
 		--  - settings (table): Override the default settings passed when initializing the server.
 		--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 		local servers = {
-			omnisharp = {},
+			csharp_ls = {},
 			ruff = {},
 			tinymist = {
 				cmd = { "tinymist" },
