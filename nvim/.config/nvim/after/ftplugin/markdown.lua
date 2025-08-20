69eb179282e3d8e04994a96ca525c0249dca412f
Zihan Jin <admin@zihanjin.com>
2025-08-20 14:54:41 +1000

    Revert "nvim: move markdown keybinds to ftplugin"
    This reverts commit 7e01e71fd7ccee946413b12c57609ee57f5eaac1.

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -1,12 +1,4 @@
--- opts
+------ opts
 vim.opt_local.spell = true
 vim.opt_local.signcolumn = "no"
 vim.g.markdown_fenced_languages = { "javascript", "typescript", "bash", "lua", "go", "rust", "c", "cpp" }
-
--- mappings
-local map = function(mode, keys, func, desc)
-	vim.keymap.set(mode, keys, func, { buffer = bufnr, desc = "Markdown: " .. desc })
-end
-
-map("n", "<C-k>", "z=", "Spell Suggest")
-map("n", "<leader>tp", ":PencilToggle<cr>", "[T]oggle [P]encil")
