c8aeef765bb8a383ba6230d419a426779fb3fc36
Zihan Jin <admin@zihanjin.com>
2025-08-20 17:22:48 +1000

    nvim: redo keymaps to conform to new neovim standard and add diagnostic config

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -4,22 +4,7 @@ return { -- LSP Configuration & Plugins
 		-- Automatically install LSPs and related tools to stdpath for neovim
 		{
 			"mason-org/mason.nvim",
-			opts = {
-
-				log_level = vim.log.levels.INFO,
-				pip = {
-					---@since 1.0.0
-					-- Whether to upgrade pip to the latest version in the virtual environment before installing packages.
-					upgrade_pip = true,
-
-					---@since 1.0.0
-					-- These args will be added to `pip install` calls. Note that setting extra args might impact intended behavior
-					-- and is not recommended.
-					--
-					-- Example: { "--proxy", "https://proxyserver" }
-					install_args = {},
-				},
-			},
+			opts = {},
 		},
 		"mason-org/mason-lspconfig.nvim",
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
@@ -68,50 +53,51 @@ return { -- LSP Configuration & Plugins
 				--
 				-- In this case, we create a fn that lets us more easily define mappings specific
 				-- for LSP related items. It sets the mode, buffer and description for us each time.
-				local map = function(keys, func, desc)
+				local map = function(keys, func, desc, mode)
+					mode = mode or "n"
 					vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
 				end
 
+				-- Rename the variable under your cursor
+				--  Most Language Servers support renaming across files, etc.
+				map("grn", vim.lsp.buf.rename, "[R]e[n]ame")
 				-- Jump to the definition of the word under your cursor.
 				--  This is where a variable was first declared, or where a fn is defined, etc.
 				--  To jump back, press <C-T>.
-				map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+				map("grd", require("telescope.builtin").lsp_definitions, "[G]oto [d]efinition")
+
+				--  This is not Goto Definition, this is Goto Declaration.
+				--  For example, in C this would take you to the header
+				map("grD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
 				-- Find references for the word under your cursor.
-				map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+				map("grr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
 
 				-- Jump to the implementation of the word under your cursor.
 				--  Useful when your language has ways of declaring types without an actual implementation.
-				map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+				map("gri", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
 
 				-- Jump to the type of the word under your cursor.
 				--  Useful when you're not sure what type a variable is and you want to see
 				--  the definition of its *type*, not where it was *defined*.
-				map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+				map("grt", require("telescope.builtin").lsp_type_definitions, "[G]oto  [T]ype Definition")
 
 				-- Fuzzy find all the symbols in your current document.
 				--  Symbols are things like variables, functions, types, etc.
-				map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+				map("gO", require("telescope.builtin").lsp_document_symbols, "[O]pen Document Symbols")
 
 				-- Fuzzy find all the symbols in your current workspace
 				--  Similar to document symbols, except searches over your whole project.
-				map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
-
-				-- Rename the variable under your cursor
-				--  Most Language Servers support renaming across files, etc.
-				map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+				map("gW", require("telescope.builtin").lsp_dynamic_workspace_symbols, "Open [W]orkspace Symbols")
 
 				-- Execute a code action, usually your cursor needs to be on top of an error
 				-- or a suggestion from your LSP for this to activate.
 				map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+				map("gra", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
 
 				-- Opens a popup that displays documentation about the word under your cursor
 				--  See `:help K` for why this keymap
 				-- map("K", vim.lsp.buf.hover, "Hover Documentation")
-
-				--  This is not Goto Definition, this is Goto Declaration.
-				--  For example, in C this would take you to the header
-				map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
 				-- The following two autocommands are used to highlight references of the
 				-- word under your cursor when your cursor rests there for a little while.
@@ -190,6 +176,33 @@ return { -- LSP Configuration & Plugins
 					end, "[T]oggle Inlay [H]ints")
 				end
 			end,
+		})
+
+		vim.diagnostic.config({
+			severity_sort = true,
+			float = { border = "rounded", source = "if_many" },
+			underline = { severity = vim.diagnostic.severity.ERROR },
+			signs = vim.g.have_nerd_font and {
+				text = {
+					[vim.diagnostic.severity.ERROR] = "󰅚 ",
+					[vim.diagnostic.severity.WARN] = "󰀪 ",
+					[vim.diagnostic.severity.INFO] = "󰋽 ",
+					[vim.diagnostic.severity.HINT] = "󰌶 ",
+				},
+			} or {},
+			virtual_text = {
+				source = "if_many",
+				spacing = 2,
+				format = function(diagnostic)
+					local diagnostic_message = {
+						[vim.diagnostic.severity.ERROR] = diagnostic.message,
+						[vim.diagnostic.severity.WARN] = diagnostic.message,
+						[vim.diagnostic.severity.INFO] = diagnostic.message,
+						[vim.diagnostic.severity.HINT] = diagnostic.message,
+					}
+					return diagnostic_message[diagnostic.severity]
+				end,
+			},
 		})
 
 		-- LSP servers and clients are able to communicate to each other what features they support.
