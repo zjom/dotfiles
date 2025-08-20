8ba8afb03a6c94188cd161b89bb72d7de40b8b03
Zihan Jin <admin@zihanjin.com>
2025-08-20 18:42:01 +1000

    nvim: cmp add luasnip

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -1,56 +1,66 @@
-return {
+return { -- Autocompletion
 	"saghen/blink.cmp",
-	-- optional: provides snippets for the snippet source
-	dependencies = {
-		"L3MON4D3/LuaSnip",
-		version = "2.*",
-		build = (function()
-			-- Build Step is needed for regex support in snippets.
-			-- This step is not supported in many windows environments.
-			-- Remove the below condition to re-enable on windows.
-			if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
-				return
-			end
-			return "make install_jsregexp"
-		end)(),
-		dependencies = {
-			-- `friendly-snippets` contains a variety of premade snippets.
-			--    See the README about individual language/framework/plugin snippets:
-			--    https://github.com/rafamadriz/friendly-snippets
-			-- {
-			--   'rafamadriz/friendly-snippets',
-			--   config = function()
-			--     require('luasnip.loaders.from_vscode').lazy_load()
-			--   end,
-			-- },
-		},
-		opts = {},
-	},
-	"folke/lazydev.nvim",
-
-	-- use a release tag to download pre-built binaries
+	event = "VimEnter",
 	version = "1.*",
-	-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-	-- build = 'cargo build --release',
-	-- If you use nix, you can build from source using latest nightly rust with:
-	-- build = 'nix run .#build-plugin',
-
-	---@module 'blink.cmp'
-	---@type blink.cmp.Config
+	dependencies = {
+		-- Snippet Engine
+		{
+			"L3MON4D3/LuaSnip",
+			version = "2.*",
+			build = (function()
+				-- Build Step is needed for regex support in snippets.
+				-- This step is not supported in many windows environments.
+				-- Remove the below condition to re-enable on windows.
+				if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+					return
+				end
+				return "make install_jsregexp"
+			end)(),
+			dependencies = {
+				-- `friendly-snippets` contains a variety of premade snippets.
+				--    See the README about individual language/framework/plugin snippets:
+				--    https://github.com/rafamadriz/friendly-snippets
+				-- {
+				--   'rafamadriz/friendly-snippets',
+				--   config = function()
+				--     require('luasnip.loaders.from_vscode').lazy_load()
+				--   end,
+				-- },
+			},
+			opts = {},
+		},
+		"folke/lazydev.nvim",
+	},
+	--- @module 'blink.cmp'
+	--- @type blink.cmp.Config
 	opts = {
-		-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-		-- 'super-tab' for mappings similar to vscode (tab to accept)
-		-- 'enter' for enter to accept
-		-- 'none' for no mappings
-		--
-		-- All presets have the following mappings:
-		-- C-space: Open menu or open docs if already open
-		-- C-n/C-p or Up/Down: Select next/previous item
-		-- C-e: Hide menu
-		-- C-k: Toggle signature help (if signature.enabled = true)
-		--
-		-- See :h blink-cmp-config-keymap for defining your own keymap
-		keymap = { preset = "default" },
+		keymap = {
+			-- 'default' (recommended) for mappings similar to built-in completions
+			--   <c-y> to accept ([y]es) the completion.
+			--    This will auto-import if your LSP supports it.
+			--    This will expand snippets if the LSP sent a snippet.
+			-- 'super-tab' for tab to accept
+			-- 'enter' for enter to accept
+			-- 'none' for no mappings
+			--
+			-- For an understanding of why the 'default' preset is recommended,
+			-- you will need to read `:help ins-completion`
+			--
+			-- No, but seriously. Please read `:help ins-completion`, it is really good!
+			--
+			-- All presets have the following mappings:
+			-- <tab>/<s-tab>: move to right/left of your snippet expansion
+			-- <c-space>: Open menu or open docs if already open
+			-- <c-n>/<c-p> or <up>/<down>: Select next/previous item
+			-- <c-e>: Hide menu
+			-- <c-k>: Toggle signature help
+			--
+			-- See :h blink-cmp-config-keymap for defining your own keymap
+			preset = "default",
+
+			-- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
+			--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+		},
 
 		appearance = {
 			-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
@@ -58,32 +68,37 @@ return {
 			nerd_font_variant = "mono",
 		},
 
-		completion = { documentation = { auto_show = false, auto_show_delay_ms = 500 } },
+		completion = {
+			-- By default, you may press `<c-space>` to show the documentation.
+			-- Optionally, set `auto_show = true` to show the documentation after a delay.
+			documentation = { auto_show = false, auto_show_delay_ms = 500 },
+		},
 
-		-- Default list of enabled providers defined so that you can extend it
-		-- elsewhere in your config, without redefining it, due to `opts_extend`
 		sources = {
 			default = { "lsp", "path", "snippets" },
 			per_filetype = {
-				sql = { inherit_defaults = true, "dadbod" },
-				markdown = { inherit_defaults = true, "buffer" },
+				sql = { "dadbod" },
+				-- optionally inherit from the `default` sources
 				lua = { inherit_defaults = true, "lazydev" },
 			},
-
 			providers = {
 				dadbod = { module = "vim_dadbod_completion.blink" },
 				lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
 			},
 		},
+
 		snippets = { preset = "luasnip" },
 
-		-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-		-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-		-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+		-- Blink.cmp includes an optional, recommended rust fuzzy matcher,
+		-- which automatically downloads a prebuilt binary when enabled.
 		--
-		-- See the fuzzy documentation for more information
-		fuzzy = { implementation = "rust" },
+		-- By default, we use the Lua implementation instead, but you may enable
+		-- the rust implementation via `'prefer_rust_with_warning'`
+		--
+		-- See :h blink-cmp-config-fuzzy for more information
+		fuzzy = { implementation = "lua" },
+
+		-- Shows a signature help window while you type arguments for a function
 		signature = { enabled = true },
 	},
-	opts_extend = { "sources.default" },
 }
--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -209,8 +209,7 @@ return { -- LSP Configuration & Plugins
 		--  By default, Neovim doesn't support everything that is in the LSP Specification.
 		--  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
 		--  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
-		local capabilities = vim.lsp.protocol.make_client_capabilities()
-		capabilities = vim.tbl_deep_extend("force", capabilities, require("blink.cmp").get_lsp_capabilities({}, false))
+		local capabilities = require("blink.cmp").get_lsp_capabilities()
 
 		-- Enable the following language servers
 		--  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
