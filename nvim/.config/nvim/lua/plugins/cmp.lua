ff040a8a05b271ce1a5dc7bbdd93479f3e3f972c
Zihan Jin <admin@zihanjin.com>
2025-08-20 14:48:30 +1000

    nvim: remove snippets and buffer from cmp

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -1,7 +1,7 @@
 return {
 	"saghen/blink.cmp",
 	-- optional: provides snippets for the snippet source
-	dependencies = { "rafamadriz/friendly-snippets" },
+	-- dependencies = { "rafamadriz/friendly-snippets" },
 
 	-- use a release tag to download pre-built binaries
 	version = "1.*",
@@ -39,7 +39,7 @@ return {
 		-- Default list of enabled providers defined so that you can extend it
 		-- elsewhere in your config, without redefining it, due to `opts_extend`
 		sources = {
-			default = { "lsp", "path", "snippets", "buffer" },
+			default = { "lsp", "path" },
 		},
 
 		-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
