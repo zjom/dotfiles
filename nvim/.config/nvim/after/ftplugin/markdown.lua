abc01fa99e508d308b3dff75f0a3fb3aa867cc3c
Zihan Jin <admin@zihanjin.com>
2025-12-22 10:50:44 +1100

    nvim: toggle preview shortcut for typst and markdown

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -2,3 +2,9 @@
 vim.opt_local.spell = true
 vim.opt_local.signcolumn = "no"
 vim.g.markdown_fenced_languages = { "javascript", "typescript", "bash", "lua", "go", "rust", "c", "cpp" }
+
+local map = function(mode, keys, func, desc)
+	vim.keymap.set(mode, keys, func, { buffer = true, desc = "Go: " .. desc })
+end
+
+map("n", "<leader>tp", ":RenderMarkdown toggle<enter>", "[T]oggle [P]review")
--- nvim/.config/nvim/after/ftplugin/typst.lua
+++ nvim/.config/nvim/after/ftplugin/typst.lua
@@ -1,1 +1,7 @@
 vim.opt_local.spell = true
+
+local map = function(mode, keys, func, desc)
+	vim.keymap.set(mode, keys, func, { buffer = true, desc = "Go: " .. desc })
+end
+
+map("n", "<leader>tp", ":TypstPreviewToggle<enter>", "[T]oggle [P]review")
