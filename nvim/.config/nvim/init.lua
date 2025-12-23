13993360f7ff5a8119436c580e1bf1f4bca9b9e9
Zihan Jin <admin@zihanjin.com>
2025-12-23 12:13:40 +1100

    nvim: performance optimisations

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,7 +1,3 @@
-require("config.options")
-require("config.keymaps")
-require("config.autocmds")
-
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -12,7 +8,34 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 		error("Error cloning lazy.nvim:\n" .. out)
 	end
 end
+
+require("config.options")
+require("config.keymaps")
+require("config.autocmds")
+
+local plugins = 'plugins'
+
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup({
-	{ import = "plugins" },
+
+require('lazy').setup(plugins, {
+	-- Don't bother me when tweaking plugins.
+	change_detection = { notify = false },
+	-- None of my plugins use luarocks so disable this.
+	rocks = {
+		enabled = false,
+	},
+	performance = {
+		rtp = {
+			-- Stuff I don't use.
+			disabled_plugins = {
+				'gzip',
+				'netrwPlugin',
+				'rplugin',
+				'tarPlugin',
+				'tohtml',
+				'tutor',
+				'zipPlugin',
+			},
+		},
+	},
 })
