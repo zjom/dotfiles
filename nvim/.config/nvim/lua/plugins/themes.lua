7e924ccdc0ec1a5bf51c0d5a019554f0634f8a26
Zihan Jin <admin@zihanjin.com>
2025-12-26 14:50:11 +1100

    nvim: add mellifluous for light theme

--- nvim/.config/nvim/lua/plugins/themes.lua
+++ nvim/.config/nvim/lua/plugins/themes.lua
@@ -12,20 +12,18 @@ return {
 			vim.cmd(":hi statusline guibg=NONE")
 		end,
 	},
-	-- {
-	-- 	"ramojus/mellifluous.nvim",
-	-- 	name = "mellifluous",
-	-- 	lazy = false,
-	-- 	priority = 1000,
-	-- 	config = function()
-	-- 		require("mellifluous").setup({
-	-- 			transparent = true,
-	-- 		})
-	--
-	-- 		vim.cmd("colorscheme mellifluous")
-	-- 		vim.cmd(":hi statusline guibg=NONE")
-	-- 	end,
-	-- },
+	{
+		"ramojus/mellifluous.nvim",
+		name = "mellifluous",
+		config = function()
+			require("mellifluous").setup({
+				mellifluous = {
+					neutral = true,
+				},
+			})
+			vim.o.background = "light"
+		end,
+	},
 	-- {
 	-- 	"catppuccin/nvim",
 	-- 	name = "catppuccin",
