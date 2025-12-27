f4fb9e5e8fd0e73e43c333876b962b87fcd4714c
Zihan Jin <admin@zihanjin.com>
2025-12-27 12:48:51 +1100

    nvim: fix ft keymap desc

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -4,7 +4,8 @@ vim.opt_local.signcolumn = "no"
 vim.g.markdown_fenced_languages = { "javascript", "typescript", "bash", "lua", "go", "rust", "c", "cpp" }
 
 local map = function(mode, keys, func, desc)
-	vim.keymap.set(mode, keys, func, { buffer = true, desc = "Go: " .. desc })
+	vim.keymap.set(mode, keys, func, { buffer = true, desc = "Markdown: " .. desc })
 end
 
-map("n", "<leader>xp", ":RenderMarkdown toggle<enter>", "Toggle Markdown [P]review")
+map("n", "<leader>xp", ":RenderMarkdown toggle<enter>", "Toggle [P]review")
+map("n", "<C-k>", "z=", "Spell Suggest")
--- nvim/.config/nvim/after/ftplugin/typst.lua
+++ nvim/.config/nvim/after/ftplugin/typst.lua
@@ -1,7 +1,7 @@
 vim.opt_local.spell = true
 
 local map = function(mode, keys, func, desc)
-	vim.keymap.set(mode, keys, func, { buffer = true, desc = "Go: " .. desc })
+	vim.keymap.set(mode, keys, func, { buffer = true, desc = "Typst: " .. desc })
 end
 
-map("n", "<leader>xp", ":TypstPreviewToggle<enter>", "Toggle Typst [P]review")
+map("n", "<leader>xp", ":TypstPreviewToggle<enter>", "Toggle [P]review")
