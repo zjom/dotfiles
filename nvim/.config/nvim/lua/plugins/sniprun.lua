d27f5a0dd65d6a5c31e17f54f84231139627e9de
Zihan Jin <admin@zihanjin.com>
2025-12-27 17:02:56 +1100

    nvim: remove sniprun

--- nvim/.config/nvim/lua/plugins/sniprun.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"michaelb/sniprun",
-	run = "bash ./install.sh",
-
-	vim.keymap.set("n", "<Leader>cr", ":SnipRun<CR>", { desc = "[C]ode [R]un", silent = true }),
-	vim.keymap.set("v", "<Leader>cr", ":SnipRun<CR>", { desc = "[C]ode [R]un", silent = true }),
-}
