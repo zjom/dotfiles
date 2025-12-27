e30a3f38242682c9d88422f25847b1ca489806f0
Zihan Jin <admin@zihanjin.com>
2025-12-27 12:49:22 +1100

    nvim: lsp

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -95,6 +95,8 @@ return { -- LSP Configuration & Plugins
 				map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
 				map("gra", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
 
+				map("<leader>e", vim.diagnostic.open_float, "[E]xpand Diagnositic")
+
 				-- Opens a popup that displays documentation about the word under your cursor
 				--  See `:help K` for why this keymap
 				-- map("K", vim.lsp.buf.hover, "Hover Documentation")
@@ -171,9 +173,9 @@ return { -- LSP Configuration & Plugins
 					client
 					and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf)
 				then
-					map("<leader>th", function()
+					map("<leader>xh", function()
 						vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
-					end, "[T]oggle Inlay [H]ints")
+					end, "Toggle Inlay [H]ints")
 				end
 			end,
 		})
@@ -298,6 +300,7 @@ return { -- LSP Configuration & Plugins
 				},
 			},
 			jdtls = {},
+			ty = {},
 		}
 
 		-- Ensure the servers and tools above are installed
