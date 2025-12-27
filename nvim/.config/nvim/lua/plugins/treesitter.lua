1a575b1d6fa5b1146136eb4505809da0b79b2e4a
Zihan Jin <admin@zihanjin.com>
2025-12-27 16:50:53 +1100

    nvim: treesitter refactor

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,51 +1,103 @@
-return { -- Highlight, edit, and navigate code
-	"nvim-treesitter/nvim-treesitter",
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter-context",
-	},
-	build = ":TSUpdate",
-	config = function()
-		-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-
-		---@diagnostic disable-next-line: missing-fields
-		require("nvim-treesitter.configs").setup({
+-- Highlight, edit, and navigate code.
+return {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = {
+			{
+				"nvim-treesitter/nvim-treesitter-context",
+				opts = {
+					-- Avoid the sticky context from growing a lot.
+					max_lines = 3,
+					-- Match the context lines to the source code.
+					multiline_threshold = 1,
+					-- Disable it when the window is too small.
+					min_window_height = 20,
+				},
+				keys = {
+					{
+						"[c",
+						function()
+							-- Jump to previous change when in diffview.
+							if vim.wo.diff then
+								return "[c"
+							else
+								vim.schedule(function()
+									require("treesitter-context").go_to_context()
+								end)
+								return "<Ignore>"
+							end
+						end,
+						desc = "Jump to upper context",
+						expr = true,
+					},
+				},
+			},
+		},
+		version = false,
+		build = ":TSUpdate",
+		opts = {
 			ensure_installed = {
 				"bash",
 				"c",
+				"cpp",
+				"fish",
+				"gitcommit",
+				"go",
+				"graphql",
 				"html",
+				"hyprlang",
+				"java",
+				"javascript",
+				"json",
+				"json5",
+				"jsonc",
 				"lua",
 				"markdown",
+				"markdown_inline",
+				"python",
+				"query",
+				"rasi",
+				"regex",
+				"rust",
+				"scss",
+				"toml",
+				"tsx",
+				"typescript",
 				"vim",
 				"vimdoc",
-				"go",
-				"gomod",
-				"gowork",
-				"python",
-				"java",
+				"yaml",
 			},
-			-- Autoinstall languages that are not installed
-			auto_install = true,
 			highlight = { enable = true },
-			indent = { enable = true },
 			incremental_selection = {
 				enable = true,
 				keymaps = {
-					init_selection = "<C-n>",
-					node_incremental = "<C-n>",
-					scope_incremental = "<C-s>",
-					node_decremental = "<C-p>",
+					init_selection = "<cr>",
+					node_incremental = "<cr>",
+					scope_incremental = false,
+					node_decremental = "<bs>",
 				},
 			},
-		})
-		vim.keymap.set("n", "[c", function()
-			require("treesitter-context").go_to_context(vim.v.count1)
-		end, { silent = true })
+			indent = {
+				enable = true,
+				-- Treesitter unindents Yaml lists for some reason.
+				disable = { "yaml" },
+			},
+		},
+		config = function(_, opts)
+			local toggle_inc_selection_group =
+				vim.api.nvim_create_augroup("mariasolos/toggle_inc_selection", { clear = true })
+			vim.api.nvim_create_autocmd("CmdwinEnter", {
+				desc = "Disable incremental selection when entering the cmdline window",
+				group = toggle_inc_selection_group,
+				command = "TSBufDisable incremental_selection",
+			})
+			vim.api.nvim_create_autocmd("CmdwinLeave", {
+				desc = "Enable incremental selection when leaving the cmdline window",
+				group = toggle_inc_selection_group,
+				command = "TSBufEnable incremental_selection",
+			})
 
-		-- There are additional nvim-treesitter modules that you can use to interact
-		-- with nvim-treesitter. You should go explore a few and see what interests you:
-		--
-		--    - Incremental selection: Included, see :help nvim-treesitter-incremental-selection-mod
-		--    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-		--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-	end,
+			require("nvim-treesitter.configs").setup(opts)
+		end,
+	},
 }
