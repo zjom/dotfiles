ae1859edd5b70a80b7e075c1b968ad9f3a818d7d
Zihan Jin <admin@zihanjin.com>
2026-02-06 13:58:35 +1100

    feat: zsh alias for tasks

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,13 +1,14 @@
 # --- Aliases ---
 alias ls='eza --icons always'
 alias tree='eza --icons always --tree '
-alias t='touch'
+alias tt='touch'
 alias c='clear'
 alias q='exit'
 alias lg='lazygit'
 alias vim='nvim'
 alias tm='tmux -2'
 alias cat='bat'
+alias t='nb t open | bat'
 
 # Function Aliases
 alias ff='open_in_nvim'
