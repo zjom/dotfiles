bafab585e7f62f7316bcc846bf7b4878c33954fa
Zihan Jin <admin@zihanjin.com>
2026-03-07 13:16:43 +1100

    fix(nvim): require missing config.commands in init

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -12,6 +12,7 @@ end
 require("config.options")
 require("config.keymaps")
 require("config.autocmds")
+require("config.commands")
 require("config.marks")
 require("config.filetype")
 require("config.lightbulb")
