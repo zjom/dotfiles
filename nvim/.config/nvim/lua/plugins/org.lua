a18279fd500d423ea2a76caab85acfd80e641d70
Zihan Jin <admin@zihanjin.com>
2026-03-12 16:41:28 +1100

    fix(nvim/org): todo keyword ordering and grouping

--- nvim/.config/nvim/lua/plugins/org.lua
+++ nvim/.config/nvim/lua/plugins/org.lua
@@ -8,10 +8,10 @@ return {
 		require("orgmode").setup({
 			org_agenda_files = "~/o/**/*",
 			org_default_notes_file = "~/o/refile.org",
-			org_todo_keywords = { "IDEA(i)", "|", "TODO(t)", "WAITING(w)", "|", "DONE(d)" },
+			org_todo_keywords = { "TODO(t)", "IDEA(i)", "WAITING(w)", "|", "DONE(d)" },
 			org_todo_keyword_faces = {
-				IDEA = ":foreground yellow",
 				TODO = ":foreground red  :weight bold",
+				IDEA = ":foreground yellow",
 				WAITING = ":foreground pink",
 				DONE = ":foreground green",
 			},
