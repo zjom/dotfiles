d8d6ab9af545e623573c0f2e1e7e4fabe39e6cca
Zihan Jin <admin@zihanjin.com>
2026-04-17 15:07:20 +1000

    chore(nvim): update treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,40 @@
 -- Highlight, edit, and navigate code.
+local ensureInstalled = {
+	"bash",
+	"c",
+	"cpp",
+	"fish",
+	"gitcommit",
+	"go",
+	"graphql",
+	"html",
+	"hyprlang",
+	"java",
+	"javascript",
+	"json",
+	"json5",
+	"jsonc",
+	"lua",
+	"markdown",
+	"markdown_inline",
+	"python",
+	"query",
+	"rasi",
+	"regex",
+	"rust",
+	"scss",
+	"toml",
+	"tsx",
+	"typescript",
+	"vim",
+	"vimdoc",
+	"yaml",
+}
+
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
 		dependencies = {
 			{
 				"nvim-treesitter/nvim-treesitter-context",
@@ -35,38 +68,28 @@ return {
 		},
 		version = false,
 		build = ":TSUpdate",
+		init = function()
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function()
+					-- Enable treesitter highlighting and disable regex syntax
+					pcall(vim.treesitter.start)
+					-- Enable treesitter-based indentation
+					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+					-- Enable treesitter-based folding
+					vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+					vim.wo[0][0].foldmethod = "expr"
+				end,
+			})
+			local alreadyInstalled = require("nvim-treesitter.config").get_installed()
+			local parsersToInstall = vim.iter(ensureInstalled)
+				:filter(function(parser)
+					return not vim.tbl_contains(alreadyInstalled, parser)
+				end)
+				:totable()
+			require("nvim-treesitter").install(parsersToInstall)
+		end,
+
 		opts = {
-			ensure_installed = {
-				"bash",
-				"c",
-				"cpp",
-				"fish",
-				"gitcommit",
-				"go",
-				"graphql",
-				"html",
-				"hyprlang",
-				"java",
-				"javascript",
-				"json",
-				"json5",
-				"jsonc",
-				"lua",
-				"markdown",
-				"markdown_inline",
-				"python",
-				"query",
-				"rasi",
-				"regex",
-				"rust",
-				"scss",
-				"toml",
-				"tsx",
-				"typescript",
-				"vim",
-				"vimdoc",
-				"yaml",
-			},
 			highlight = { enable = true },
 			incremental_selection = {
 				enable = true,
@@ -97,7 +120,7 @@ return {
 				command = "TSBufEnable incremental_selection",
 			})
 
-			require("nvim-treesitter.configs").setup(opts)
+			require("nvim-treesitter").setup(opts)
 		end,
 	},
 }
