1ff3962a4197dd4b17211730a51947a5f06e086b
Zihan Jin <admin@zihanjin.com>
2026-04-23 15:04:30 +1000

    feat(nvim): use minimal statusline

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -99,3 +99,6 @@ vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_node_provider = 0
+
+vim.o.statusline =
+	"%<%f %h%w%m%r %y %{% v:lua.require('vim._core.util').term_exitcode() %}%=%{% luaeval('(package.loaded[''vim.ui''] and vim.api.nvim_get_current_win() == tonumber(vim.g.actual_curwin or -1) and vim.ui.progress_status()) or '''' ')%}%{% &showcmdloc == 'statusline' ? '%-10.S ' : '' %}%{% exists('b:keymap_name') ? '<'..b:keymap_name..'> ' : '' %}%{% &busy > 0 ? '◐ ' : '' %}%{% luaeval('(package.loaded[''vim.diagnostic''] and next(vim.diagnostic.count()) and vim.diagnostic.status() .. '' '') or '''' ') %}%{% &ruler ? ( &rulerformat == '' ? '%-14.(%l,%c%V%) %P' : &rulerformat ) : '' %}"
--- nvim/.config/nvim/lua/plugins/mini-statusline.lua
+++
@@ -1,8 +0,0 @@
--- Simple and easy statusline.
-return {
-	"nvim-mini/mini.statusline",
-	config = function()
-		require("mini.statusline").setup()
-	end,
-	event = "BufWinEnter",
-}
