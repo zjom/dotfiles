26ea1874d57c1c45dec8b81171258c5d4bba7b7a
Zihan Jin <admin@zihanjin.com>
2026-05-08 00:54:09 +1000

    feat(nvim): rust analyzer per project config override

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -105,6 +105,22 @@ vim.api.nvim_create_autocmd("LspAttach", {
 	end,
 })
 
+-- Rust analyzer doesn't really support per project/ workspace config.
+-- This is a hacky workaround.
+local function get_project_rustanalyzer_settings()
+	local handle = io.open(vim.fn.resolve(vim.fn.getcwd() .. "/./.rust-analyzer.json"))
+	if not handle then
+		return {}
+	end
+	local out = handle:read("*a")
+	handle:close()
+	local config = vim.json.decode(out)
+	if type(config) == "table" then
+		return config
+	end
+	return {}
+end
+
 local servers = {
 	roslyn = {},
 	ruff = {},
@@ -181,11 +197,20 @@ local servers = {
 	basedpyright = {},
 	rust_analyzer = {
 		settings = {
-			["rust-analyzer"] = {
-				check = {
-					command = "clippy",
+			["rust-analyzer"] = vim.tbl_deep_extend(
+				"force",
+				{
+					check = {
+						command = "clippy",
+					},
 				},
-			},
+				get_project_rustanalyzer_settings(),
+				{
+					-- Overrides (forces these regardless of what's in .rust-analyzer.json
+					-- procMacro = { enable = true },
+					-- diagnostics = { disabled = { "inactive-code" } },
+				}
+			),
 		},
 	},
 }
