4392a0b1fbd6fb90b0d995f056888d2c0a2a4bf7
Zihan Jin <admin@zihanjin.com>
2026-05-08 11:43:51 +1000

    misc(nvim): formatting

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -8,7 +8,7 @@ vim.cmd("colorscheme vague")
 vim.cmd(":hi statusline guibg=NONE")
 
 -- Iterate over all Lua files in the plugins directory and load them
-local plugins_dir = vim.fs.joinpath(vim.fn.stdpath("config"), "lua","plugins")
+local plugins_dir = vim.fs.joinpath(vim.fn.stdpath("config"), "lua", "plugins")
 for file_name, type in vim.fs.dir(plugins_dir) do
 	if type == "file" and file_name:match("%.lua$") and file_name ~= "init.lua" then
 		local module = file_name:gsub("%.lua$", "")
