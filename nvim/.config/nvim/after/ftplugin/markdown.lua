1f47a396bab2ca18912122fb6e0839118ae856bf
Zihan Jin <admin@zihanjin.com>
2026-05-08 13:14:59 +1000

    misc(nvim): cleanup ft markdown

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -3,8 +3,4 @@ vim.opt_local.spell = true
 vim.opt_local.signcolumn = "no"
 vim.g.markdown_fenced_languages = { "javascript", "typescript", "bash", "lua", "go", "rust", "c", "cpp" }
 
-local map = function(mode, keys, func, desc)
-	vim.keymap.set(mode, keys, func, { buffer = true, desc = "Markdown: " .. desc })
-end
-
-map("n", "<C-k>", "z=", "Spell Suggest")
+vim.keymap.set("n", "<C-k>", "z=", { desc = "Spell Suggest", buffer = true })
