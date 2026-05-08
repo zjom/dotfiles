0f16e3274889c5481ba5d2358e8a43c410dfb0d8
Zihan Jin <admin@zihanjin.com>
2026-05-08 13:42:02 +1000

    feat(nvim): add extra trouble keymaps

--- nvim/.config/nvim/lua/plugins/trouble.lua
+++ nvim/.config/nvim/lua/plugins/trouble.lua
@@ -1,5 +1,17 @@
 vim.pack.add({ "https://github.com/folke/trouble.nvim" })
 require("trouble").setup()
 vim.keymap.set("n", "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", {
-	desc = "Diagnostics (Trouble)",
+	desc = "[X]oggle Diagnostics (Trouble)",
+})
+vim.keymap.set("n", "<leader>xl", "<cmd>Trouble loclist toggle<cr>", {
+	desc = "[X]oggle [L]oclist (Trouble)",
+})
+vim.keymap.set("n", "<leader>xq", "<cmd>Trouble qflist toggle<cr>", {
+	desc = "[X]oggle [Q]uickfix List (Trouble)",
+})
+vim.keymap.set("n", "<leader>cs", "<cmd>Trouble symbols toggle focus=false<cr>", {
+	desc = "[C]ode [S]ymbols (Trouble)",
+})
+vim.keymap.set("n", "<leader>cl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", {
+	desc = "[C]ode [L]SP Definitions / references / ... (Trouble)",
 })
