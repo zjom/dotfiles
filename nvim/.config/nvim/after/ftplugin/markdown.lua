af5da14c975e0f2fb96f44bdd79f014622061742
Zihan Jin <admin@zihanjin.com>
2026-05-23 21:00:25 +1000

    feat(nvim): set conceal level to 0 in markdown

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -2,5 +2,6 @@
 vim.opt_local.spell = true
 vim.opt_local.signcolumn = "no"
 vim.g.markdown_fenced_languages = { "javascript", "typescript", "bash", "lua", "go", "rust", "c", "cpp" }
+vim.o.conceallevel = 0
 
 vim.keymap.set("n", "<C-k>", "z=", { desc = "Spell Suggest", buffer = true })
