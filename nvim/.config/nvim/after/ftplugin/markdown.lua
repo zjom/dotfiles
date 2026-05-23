2de4820d574d50b4bc23969c16b1e7363e61ddc5
Zihan Jin <admin@zihanjin.com>
2026-05-23 21:00:40 +1000

    feat(nvim): add extra langauges to markdown fenced languages

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -1,7 +1,8 @@
 ------ opts
 vim.opt_local.spell = true
 vim.opt_local.signcolumn = "no"
-vim.g.markdown_fenced_languages = { "javascript", "typescript", "bash", "lua", "go", "rust", "c", "cpp" }
+vim.g.markdown_fenced_languages =
+	{ "javascript", "typescript", "bash", "lua", "go", "rust", "c", "cpp", "python", "typst" }
 vim.o.conceallevel = 0
 
 vim.keymap.set("n", "<C-k>", "z=", { desc = "Spell Suggest", buffer = true })
