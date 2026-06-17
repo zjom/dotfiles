56a842d4633365e6e718c5d73f613c97f3b3318b
Zihan Jin <admin@zihanjin.com>
2026-06-17 16:47:50 +1000

    feat(nvim): typst

--- nvim/.config/nvim/after/ftplugin/typst.lua
+++ nvim/.config/nvim/after/ftplugin/typst.lua
@@ -1,2 +1,4 @@
+require("typst-preview").setup({ open_cmd = 'open -a "Google Chrome" %s' })
 vim.opt_local.spell = true
 vim.keymap.set("n", "<C-k>", "z=", { desc = "Spell Suggest", buffer = true })
+vim.keymap.set("n", "<leader>xp", ":TypstPreviewToggle<enter>", { desc = "[X]oggle TypstPreview" })
