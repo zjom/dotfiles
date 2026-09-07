739b05cc9c1ddc49aeeef2df1067115be9f2c0bc
Zihan Jin <admin@zihanjin.com>
2026-09-07 10:18:33 +0000

    misc(nix): git config

--- nix/home.nix
+++ nix/home.nix
@@ -37,9 +37,14 @@ in
 
   programs.git = {
     enable = true;
-    settings.user = {
-      name = "Zihan Jin";
-      email = "admin@zihanjin.com";
+    settings = {
+      user = {
+        name = "Zihan Jin";
+        email = "admin@zihanjin.com";
+      };
+      init = {
+        defaultBranch = "main";
+      };
     };
   };
 
