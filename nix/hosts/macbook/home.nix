d6a9668c66343e4e7f7b41ed00164ae81bea3c14
Zihan Jin <admin@zihanjin.com>
2026-09-14 16:30:43 +1000

    misc: set this as primary

--- nix/hosts/macbook/home.nix
+++ nix/hosts/macbook/home.nix
@@ -12,11 +12,6 @@
   home.username = username;
   home.homeDirectory = "/Users/${username}";
 
-  # This machine keeps the nix branch of the dotfiles repository in a second
-  # checkout, because ~/dotfiles is still the older stow based main branch.
-  # Delete this line once there is one checkout at ~/dotfiles, as on WSL.
-  my.dotfilesRoot = "${config.home.homeDirectory}/nix-dotfiles";
-
   my.shell.aliases.rebuild = "sudo darwin-rebuild switch --flake '${config.my.flakeRoot}#${hostName}'";
 
   programs.aerospace = {
