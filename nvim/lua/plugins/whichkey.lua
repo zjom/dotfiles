764dc6e3059e6a04a8179ec86f8bac9de57b20d4
Zihan Jin <admin@zihanjin.com>
2026-09-19 20:58:48 +1000

    fix(nvim): remove unused Request spec in whichkey

--- nvim/lua/plugins/whichkey.lua
+++ nvim/lua/plugins/whichkey.lua
@@ -1,12 +1,13 @@
 vim.pack.add({ "https://github.com/folke/which-key.nvim" })
 require("which-key").setup({
 	-- Document existing key chains
+	---@type wk.Spec
 	spec = {
 		{ "<leader>b", group = "[B]uffer" },
 		{ "<leader>c", group = "[C]ode" },
 		{ "<leader>d", group = "[D]ap" },
-		{ "<leader>r", group = "[R]equest" },
 		{ "<leader><C-R>", group = "[R]estart" },
+		{ "<leader>L", group = "[L]ine" },
 		{ "<leader>s", group = "[S]earch" },
 		{ "<leader>w", group = "[W]orkspace" },
 		{ "<leader>x", group = "[X]oggle" },
